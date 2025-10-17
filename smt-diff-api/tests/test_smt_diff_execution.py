a741313849d897256f71e1d42180e8b81e5d4e27
soaibsafi <soaib.safi@gmail.com>
2025-10-17 16:35:19 +0200

    🧪Updated the test cases for smt diff api

--- smt-diff-api/tests/test_smt_diff_execution.py
+++ smt-diff-api/tests/test_smt_diff_execution.py
@@ -1,38 +1,41 @@
 from smt_diff import *
 
-s1 = """
-(declare-const x Real)
-(assert (> x 0))
-(assert (is_int x))
-"""
-
-s2 = """
-(declare-const x Real)
-(assert (> x 0.5))"""
-
 
 def test_int_real_diff():
-    id = get_next_witness(s2, s1, mode="diff")
-    witness = get_next_witness(id)
-    print(witness)
+    s1 = """
+    (declare-const x Real)
+    (assert (> x 0))
+    (assert (is_int x))
+    """
+
+    s2 = """
+    (declare-const x Real)
+    (assert (> x 0.5))"""
+    generator = diff_witness(parse_smt2_string(s2), parse_smt2_string(s1))
+
+    witness = next(generator).sexpr()
     assert "Real" in witness
-    
-    
+
+
 def test_datatype_changes():
     s1 = """
-    ; (declare-datatypes () ((S A B)))
-    (declare-datatypes () ((S A B C)))
+    (declare-datatypes () ((S A B )))
+    ;(declare-datatypes () ((S A B C)))
     (declare-const x S)
-    (assert (< x C))"""
+    (assert (= x A))
+    ;(assert (< x C))"""
     s2 = """
     (declare-datatypes () ((S A B C)))
-    (declare-const x S)"""
-    
-    id = get_next_witness(s1, s2, mode="diff")
-    witness = get_next_witness(id)
+    (declare-const y S)
+    (assert (= y B))"""
+    s1_assertions = parse_smt2_string(s1)
+    s2_assertions = parse_smt2_string(s2)
+    generator = diff_witness(s1_assertions, s2_assertions)
+    witness = next(generator).sexpr()
     print(witness)
     assert "D" not in witness
-    
+
+
 def test_store_witness():
     s1 = """
     (declare-const x Int)
@@ -43,8 +46,8 @@ def test_store_witness():
     (declare-const x Int)
     (assert (> x 3))
     """
-    
-    id = store_witness(s1, s2, mode="diff")
-    witness1 = get_next_witness(id)
-    witness2 = get_next_witness(id)
+
+    generator = diff_witness(parse_smt2_string(s1), parse_smt2_string(s2))
+    witness1 = next(generator).sexpr()
+    witness2 = next(generator).sexpr()
     assert witness1 != witness2
